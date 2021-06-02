0752e17cac3e9c214d8b9f5489ff7aff46f00ce7
root <tech889hack@gmail.com>
2021-06-02 09:39:00 +0000

    02June Linux script

--- group.sh
+++ group.sh
@@ -1,27 +1,27 @@
-echo "Start"
+echo "###############  ECA Linux Shell Script Group Assignment  ###############"
+echo "######################  Script To Calculate Tokens  #####################"
+echo 
+echo "########  Tokens in given file  ########"
+echo
+echo "##############   File  #################"
 cat file1
-#sum=0
+echo "################ End  ##################"
+#varc calculate 
 varc=$(egrep -o -i -w 'int|main|a|b|10|20|;|0|return|=|,|printf' file1 | wc -l)
-#varc=$(egrep -w -c 'int|main|(|)|{|}a|b|;|=|10|return|0' file1)
-#sum=$sum+$varc
+#varp count number of '(' in given file
 varp=$(grep -c '(' file1)
+#varp2 count number of ')' in given file
 varp2=$(grep -c ')' file1)
+#varcp count number of '{' in given file
 varcp=$(grep -c '{' file1)
+#varcp2 count number of '}' in given file
 varcp2=$(grep -c '}' file1)
+#vardq count number of stings
 vardq=$(grep -c '"' file1)
+#varo count number of + operator
 varo=$(grep -c '+' file1)
+
 sum=$(($varc + $varp + $varp2 + $vardq +$varcp + $varcp2 + $varo))
+echo
+echo "Total Tokens Present : $sum"
 
-echo $sum
-echo "==========="
-if [ $varc != 0 ]
-then echo "c is not zero"
-else
-
-	echo "c is zor"
-fi
-if [[ "typo3" =~ (wordpress|meganto|typo34)$ ]]; then echo "found" 
-else
-	
-	  echo "history"
-  fi
